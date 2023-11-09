1eb47636366579c712c48764b5427bf01b257ad5
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-09 18:11:43 +0530

    feat: update client side js for group chat feature

--- server/public/app.js
+++ server/public/app.js
@@ -41,9 +41,26 @@ msgInput.addEventListener('keypress', () => {
 // Listen for messages
 socket.on('message', (data) => {
   activity.textContent = '';
+  const { name, text, time } = data;
   const li = document.createElement('li');
-  li.textContent = data;
-  document.querySelector('ul').appendChild(li);
+  li.className = 'post';
+  if (name === nameInput.value) li.className = 'post post--left';
+  if (name !== nameInput.value && name !== 'Admin')
+    li.className = 'post post--right';
+  if (name !== 'Admin') {
+    li.innerHTML = `<div class="post__header ${
+      name === nameInput.value ? 'post__header--user' : 'post__header--reply'
+    }">
+      <span class="post__header--name">${name}</span> 
+      <span class="post__header--time">${time}</span> 
+      </div>
+      <div class="post__text">${text}</div>`;
+  } else {
+    li.innerHTML = `<div class="post__text">${text}</div>`;
+  }
+  document.querySelector('.chat-display').appendChild(li);
+
+  chatDisplay.scrollTop = chatDisplay.scrollHeight;
 });
 
 let activityTimer;
@@ -56,3 +73,37 @@ socket.on('activity', (name) => {
     activity.textContent = '';
   }, 3000);
 });
+
+socket.on('userList', ({ users }) => {
+  showUsers(users);
+});
+
+socket.on('roomList', ({ rooms }) => {
+  showRooms(rooms);
+});
+
+function showUsers(users) {
+  userList.textContent = '';
+  if (users) {
+    userList.innerHTML = `<em>Users in ${chatRoom.value}: </em>`;
+    users.forEach((user, i) => {
+      userList.textContent += ` ${user.name}`;
+      if (users.length > 1 && i !== users.length - 1) {
+        userList.textContent += ',';
+      }
+    });
+  }
+}
+
+function showRooms(rooms) {
+  roomList.textContent = '';
+  if (users) {
+    roomList.innerHTML = '<em>Active Rooms: </em>';
+    rooms.forEach((room, i) => {
+      roomList.textContent += ` ${rooms.name}`;
+      if (rooms.length > 1 && i !== rooms.length - 1) {
+        roomList.textContent += ',';
+      }
+    });
+  }
+}
